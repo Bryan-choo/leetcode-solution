0228e26726430948757af6d634b95cd360c452cc
chuchengwei <chuchengwei@192.168.10.102>
2019-01-25 13:38:23 +0800

    Java 两数之和 TwoSum 01 Easy

---
+++ algorithm/java/src/数组_Array/两数之和_TwoSum__01_Easy.java
@@ -0,0 +1,41 @@
+package 数组_Array;
+
+import javax.sound.midi.Soundbank;
+
+/**
+ * 
+ * @author chuchengwei
+ *
+ */
+
+/*
+ * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+
+ * 你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+
+ * 示例:
+
+ * 给定 nums = [2, 7, 11, 15], target = 9
+
+ * 因为 nums[0] + nums[1] = 2 + 7 = 9
+ * 所以返回 [0, 1]
+ *
+ */
+
+public class 两数之和_TwoSum__01_Easy {
+	
+	
+	
+	public static void main(String[] args) {
+		Solution s = new Solution();
+		System.out.println(s.twoSum(new int[1],  2));
+	}
+
+}
+
+class Solution {
+	public Solution() {};
+    public int[] twoSum(int[] nums, int target) {
+        return new int[1];
+    }
+}
